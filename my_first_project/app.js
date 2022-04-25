fff7cc2f6e12d9dec12b7fee8e3a1dff16c2ff07
RajanBhatia777 <rajanbhatia100@gmail.com>
2022-04-25 13:06:34 +0530

    code

--- my_first_project/app.js
+++ my_first_project/app.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
+/* eslint-disable func-names */
+/* eslint-disable arrow-spacing */
+const express = require('express');
+const app = express();
 
 const PORT = 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World okok')
-})
+  res.send('Hello World okok');
+});
 
-app.listen(PORT,()=>console.log(`Servervis running on port ${PORT}`))
+app.listen(PORT,()=>console.log(`Servervis running on port ${PORT}`));
